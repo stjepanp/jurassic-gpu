91076ce415f21011ece7f46b993e85ff26d85f6b
Stjepan Pozgaj <stjepan.pozgaj1@gmail.com>
2021-09-01 12:16:40 +0200

    Connect gpu and scatter with make and libutil.a

---
+++ src/inter_folder/interface.h
@@ -0,0 +1,16 @@
+#ifndef _INTERFACE_H_
+#define _INTERFACE_H_
+
+#include "jr_scatter_gpu.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+__host__ void print_obs_size(obs_t *a);
+
+#ifdef __cplusplus
+} //extern "C"
+#endif
+
+#endif
