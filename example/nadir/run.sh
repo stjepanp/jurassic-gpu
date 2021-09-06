3a4b3f10433ac886b08df74fb30e0c5ab0a2dc6b
Stjepan Pozgaj <stjepan.pozgaj1@gmail.com>
2021-09-06 17:52:08 +0200

    update example run scripts

--- example/nadir/run.sh
+++ example/nadir/run.sh
@@ -1,3 +1,27 @@
+#!/bin/bash -x
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=16
+#SBATCH --output=out
+#SBATCH --error=err
+#SBATCH --time=1:00:00
+#SBATCH --partition=gpus
+#SBATCH --gres=gpu:1
+#SBATCH --account=slmet
+
+module load Python/3.8.5
+module load GCC/9.3.0
+module load OpenMPI/4.1.0rc1
+module load CUDA/11.3
+module load Nsight-Systems/2021.1.1
+module load Nsight-Compute/2020.3.0
+module load ParaStationMPI/5.4.7-1
+module load mpi-settings/CUDA
+module load GCCcore/.10.3.0
+module load GCCcore/.9.3.0
+module load GSL/2.6 
+
+
 #! /bin/bash
 
 # Setup...
